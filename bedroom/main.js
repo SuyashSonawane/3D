34b68b497b57ec74875749b2fd043e42e078c3c9
suyashsonawane <suyashsonawane007@gmail.com>
2020-08-12 17:07:24 +0530

    colors added

--- bedroom/main.js
+++ bedroom/main.js
@@ -31,33 +31,258 @@ let BOOK3 = "book4";
 var cameraFar = 5;
 var theModel;
 var activeOption = BACKWALL;
-const Textures = [
-  "images/carpet.jpg",
-  "images/cushion1.jpg",
-  "images/cushion2.jpg",
-  "images/floor1.jpg",
-  "images/wood.jpg",
-  "images/wood1.jpg",
-  "images/wallpaper.jpg",
-  "images/sofabase.jpg",
-];
+// const Textures = [
+//   "images/carpet.jpg",
+//   "images/cushion1.jpg",
+//   "images/cushion2.jpg",
+//   "images/floor1.jpg",
+//   "images/wood.jpg",
+//   "images/wood1.jpg",
+//   "images/wallpaper.jpg",
+//   "images/sofabase.jpg",
+// ];
+// const TRAY = document.getElementById("js-tray-slide");
+// Textures.forEach((el) => {
+//   let img = document.createElement("img");
+//   img.classList.add("tray__swatch");
+//   img.setAttribute("data-key", el);
+//   img.src = el;
+//   // img.width = 30;
+//   // img.height = 30;
+//   TRAY.append(img);
+//   // document.querySelector(".tray").appendChild(img);
+// });
+// const swatches = document.querySelectorAll(".tray__swatch");
+
+// for (const swatch of swatches) {
+//   swatch.addEventListener("click", selectSwatch);
+// }
+
+// const options = document.querySelectorAll(".option");
+
+// for (const option of options) {
+//   option.addEventListener("click", selectOption);
+// }
+
+// function selectOption(e) {
+//   let option = e.target;
+//   activeOption = e.target.dataset.option;
+//   for (const otherOption of options) {
+//     otherOption.classList.remove("--is-active");
+//   }
+//   option.classList.add("--is-active");
+// }
+
+// function selectSwatch(e) {
+//   let src = e.target.dataset.key;
+//   let txt = new THREE.TextureLoader();
+//   txt.load(src, (tx) => {
+//     let mat = new THREE.MeshPhongMaterial({
+//       map: tx,
+//       wireframe: false,
+//     });
+//     theModel.traverse((o) => {
+//       if (o.isMesh) {
+//         if (o.name == activeOption) {
+//           o.material = mat;
+//         }
+//       }
+//     });
+//   });
+
+//   // setMaterial(theModel, "legs", new_mtl);
+// }
+
+let initRotate = 0;
 const TRAY = document.getElementById("js-tray-slide");
-Textures.forEach((el) => {
-  let img = document.createElement("img");
-  img.classList.add("tray__swatch");
-  img.setAttribute("data-key", el);
-  img.src = el;
-  // img.width = 30;
-  // img.height = 30;
-  TRAY.append(img);
-  // document.querySelector(".tray").appendChild(img);
-});
+function initialRotation() {
+  initRotate++;
+  if (initRotate <= 120) {
+    theModel.rotation.y += Math.PI / 60;
+  } else {
+    loaded = true;
+  }
+}
+const DRAG_NOTICE = document.getElementById("js-drag-notice");
+if (theModel != null && loaded == false) {
+  initialRotation();
+  DRAG_NOTICE.classList.add("start");
+}
+var slider = document.getElementById("js-tray"),
+  sliderItems = document.getElementById("js-tray-slide"),
+  difference;
+
+function slide(wrapper, items) {
+  var posX1 = 0,
+    posX2 = 0,
+    posInitial,
+    threshold = 20,
+    posFinal,
+    slides = items.getElementsByClassName("tray__swatch");
+
+  // Mouse events
+  items.onmousedown = dragStart;
+
+  // Touch events
+  items.addEventListener("touchstart", dragStart);
+  items.addEventListener("touchend", dragEnd);
+  items.addEventListener("touchmove", dragAction);
+  function dragStart(e) {
+    e = e || window.event;
+    posInitial = items.offsetLeft;
+    difference = sliderItems.offsetWidth - slider.offsetWidth;
+    difference = difference * -1;
+
+    if (e.type == "touchstart") {
+      posX1 = e.touches[0].clientX;
+    } else {
+      posX1 = e.clientX;
+      document.onmouseup = dragEnd;
+      document.onmousemove = dragAction;
+    }
+  }
+
+  function dragAction(e) {
+    e = e || window.event;
+
+    if (e.type == "touchmove") {
+      posX2 = posX1 - e.touches[0].clientX;
+      posX1 = e.touches[0].clientX;
+    } else {
+      posX2 = posX1 - e.clientX;
+      posX1 = e.clientX;
+    }
+
+    if (
+      items.offsetLeft - posX2 <= 0 &&
+      items.offsetLeft - posX2 >= difference
+    ) {
+      items.style.left = items.offsetLeft - posX2 + "px";
+    }
+  }
+
+  function dragEnd(e) {
+    posFinal = items.offsetLeft;
+    if (posFinal - posInitial < -threshold) {
+    } else if (posFinal - posInitial > threshold) {
+    } else {
+      items.style.left = posInitial + "px";
+    }
+
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+slide(slider, sliderItems);
+var colorArray = [
+  "#FF6633",
+  "#FFB399",
+  "#FF33FF",
+  "#FFFF99",
+  "#00B3E6",
+  "#E6B333",
+  "#3366E6",
+  "#999966",
+  "#99FF99",
+  "#B34D4D",
+  "#80B300",
+  "#809900",
+  "#E6B3B3",
+  "#6680B3",
+  "#66991A",
+  "#FF99E6",
+  "#CCFF1A",
+  "#FF1A66",
+  "#E6331A",
+  "#33FFCC",
+  "#66994D",
+  "#B366CC",
+  "#4D8000",
+  "#B33300",
+  "#CC80CC",
+  "#66664D",
+  "#991AFF",
+  "#E666FF",
+  "#4DB3FF",
+  "#1AB399",
+  "#E666B3",
+  "#33991A",
+  "#CC9999",
+  "#B3B31A",
+  "#00E680",
+  "#4D8066",
+  "#809980",
+  "#E6FF80",
+  "#1AFF33",
+  "#999933",
+  "#FF3380",
+  "#CCCC00",
+  "#66E64D",
+  "#4D80CC",
+  "#9900B3",
+  "#E64D66",
+  "#4DB380",
+  "#FF4D4D",
+  "#99E6E6",
+  "#6666FF",
+];
+let colors = [
+  {
+    color: "91c9f6",
+  },
+  {
+    color: "514f71",
+  },
+  {
+    color: "f89598",
+  },
+  {
+    color: "3be8b6",
+  },
+  {
+    color: "6f352a",
+  },
+  {
+    color: "f8c45f",
+  },
+  {
+    color: "b8724d",
+  },
+];
+for (const c of colorArray) {
+  colors.push({ color: c.split("#")[1] });
+}
+// Function - Build Colors
+function buildColors(colors) {
+  for (let [i, color] of colors.entries()) {
+    let swatch = document.createElement("div");
+    swatch.classList.add("tray__swatch");
+
+    swatch.style.background = "#" + color.color;
+
+    swatch.setAttribute("data-key", i);
+    TRAY.append(swatch);
+  }
+}
+
+buildColors(colors);
+
 const swatches = document.querySelectorAll(".tray__swatch");
 
 for (const swatch of swatches) {
   swatch.addEventListener("click", selectSwatch);
 }
-
+function setMaterial(parent, type, mtl) {
+  parent.traverse((o) => {
+    if (o.isMesh) {
+      if (o.name == type) {
+        o.material = mtl;
+      }
+    }
+  });
+}
+// Select Option
 const options = document.querySelectorAll(".option");
 
 for (const option of options) {
@@ -72,26 +297,18 @@ function selectOption(e) {
   }
   option.classList.add("--is-active");
 }
-
 function selectSwatch(e) {
-  let src = e.target.dataset.key;
-  let txt = new THREE.TextureLoader();
-  txt.load(src, (tx) => {
-    let mat = new THREE.MeshPhongMaterial({
-      map: tx,
-      wireframe: false,
-    });
-    theModel.traverse((o) => {
-      if (o.isMesh) {
-        if (o.name == activeOption) {
-          o.material = mat;
-        }
-      }
-    });
+  let color = colors[parseInt(e.target.dataset.key)];
+  let new_mtl;
+
+  new_mtl = new THREE.MeshPhongMaterial({
+    color: parseInt("0x" + color.color),
+    shininess: color.shininess ? color.shininess : 10,
   });
 
-  // setMaterial(theModel, "legs", new_mtl);
+  setMaterial(theModel, activeOption, new_mtl);
 }
+
 const MODEL_PATH = "models/bedroom.glb";
 
 const BACKGROUND_COLOR = 0xf1f1f1;
@@ -175,12 +392,11 @@ function loadMaterials() {
   let mats = [
     [FLOOR, "images/floor1.jpg"],
     [BACKWALL, "images/wood1.jpg"],
-    [CARPET, "images/carpet.jpg"],
-    [TAbLE_TOP, "images/wood.jpg"],
-    [DECOR, "images/wood.jpg"],
-    [DECOR1, "images/cushion1.jpg"],
-    [DECOR2, "images/cushion1.jpg"],
-    [DECOR3, "images/cushion1.jpg"],
+    [BLANKET, "images/carpet.jpg"],
+    [BED_CUSHION, "images/wood.jpg"],
+    [PILLOW2, "images/cushion2.jpg"],
+    [PILLOW1, "images/cushion2.jpg"],
+    [BED, "images/cushion1.jpg"],
     [DECOR4, "images/cushion1.jpg"],
     // [BOOK1, "images/cushion2.jpg"],
     // [BOOK3, "images/cushion1.jpg"],
